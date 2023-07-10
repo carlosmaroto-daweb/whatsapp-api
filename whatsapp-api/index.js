a796158d8cc3a34dde60fcb82e8c5b42e1497902
carlosmaroto-daweb <carlosmaroto.daweb@gmail.com>
2023-07-10 14:57:50 +0200

    v1.0

---
+++ whatsapp-api/index.js
@@ -0,0 +1,93 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+const { MongoStore } = require('wwebjs-mongo');
+const { Client, RemoteAuth } = require('whatsapp-web.js');
+const QRCode = require('qrcode');
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const fs = require("fs");
+/*
+var messages = [{
+  id: 1,
+  text: "Hola soy un mensaje",
+  author: "Carlos Maroto"
+}];
+*/
+app.use(express.static('public'));
+
+server.listen(3000, function(){
+  console.log("Servidor corriendo en http://whatsapp-api:3000");
+});
+
+io.on('connection', function(socketClient){
+  console.log("Alguien se ha conectado con Sockets");
+  /*
+  socket.emit('messages', messages);
+
+  socket.on('new-message', function(data){
+    messages.push(data);
+
+    io.sockets.emit('messages', messages);
+  });
+  */
+  const MONGODB_URI = process.env.MONGODB_URI;
+
+  mongoose.connect(MONGODB_URI).then(() => {
+    console.log("Data Base: Connected");
+    const store = new MongoStore({ mongoose: mongoose });
+    const client = new Client({
+      authStrategy: new RemoteAuth({
+        store: store,
+        backupSyncIntervalMs: 300000
+      }),
+      restartOnAuthFail: true,
+      qrTimeoutMs: 0,
+      authTimeoutMs: 0,
+      takeoverOnConflict: true,
+      puppeteer: {
+        headless: true,
+        args: [
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+        '--disable-dev-shm-usage',
+        '--disable-accelerated-2d-canvas',
+        '--no-first-run',
+        '--no-zygote',
+        '--disable-gpu',
+        '--unhandled-rejections=strict'
+        ],
+      }
+    });
+    client.initialize();
+    console.log("Cliente inicializado");
+
+    client.on('qr', (qr) => {
+      QRCode.toFile('./img/qr.png', qr, { errorCorrectionLevel: 'H' }, 
+      function(err) {
+        if(err) throw err;
+        console.log('QR code generate');
+        const img = fs.readFileSync("./img/qr.png", "base64");
+        socketClient.emit('qr', img);
+      });
+    });
+    
+    client.on('ready', () => {
+      console.log('Client is ready!');
+      socketClient.emit('client-ready');
+    });
+
+    client.on('remote_session_saved', () => {
+      console.log('Session is saved!');
+    });
+    
+    client.on('message', (message) => {
+      console.log(message)
+      if(message.body === 'info') {
+        let text = 'Hola! Soy el asistente virtual de Academia Cartabón, ahora mismo estamos en mantenimiento, conctactanos más tarde.';
+        client.sendMessage(message.from, text);
+      }
+    });
+  });
+});
---
+++ whatsapp-api/public/js/index.js
@@ -0,0 +1,90 @@
+const socket = io.connect('http://localhost:3000', {'forceNew' : true});
+
+socket.on('qr', function(data) {
+    document.getElementsByClassName('qr-img')[0].style.width = "340px";
+    document.getElementsByClassName('qr-img')[0].style.height = "340px";
+    document.getElementsByClassName('qr-img')[0].style.background = 'url(data:image/png;base64,'+data+')';
+});
+
+socket.on('client-ready', function() {
+    document.getElementsByClassName('whatsapp-qr')[0].style.display = "none";
+    document.getElementsByClassName('whatsapp-container')[0].style.display = "flex";
+
+    const themeMode = document.getElementsByClassName('theme-mode')[0];
+    themeMode.addEventListener("change", function(){
+        if (themeMode.checked == true){
+            let styles = `
+                --background-primary: #111b21;
+                --background-secondary: #182229;
+                --background-tertiary: #202c33;
+                --background-fourth: #2a3942;
+                --background-fifth: #ffffff29;
+                --chat-background: #0b141a;
+                --border-primary: #111b21;
+                --border-secondary: #6b778056;
+                --text-primary: #e9edef;
+                --text-secondary: #d1d7db;
+                --text-tertiary: #8696a0;
+                --icon-primary: #aebac1;
+                --unseen-messages: #00a884;
+                --message-background-primary: #005c4b;
+                --message-background-secondary: #202c33;
+                --theme-mode: #00a884;
+                --filter-primary: invert(11%) sepia(14%) saturate(1386%) hue-rotate(158deg) brightness(94%) contrast(85%);
+                --filter-secondary: invert(24%) sepia(86%) saturate(653%) hue-rotate(126deg) brightness(95%) contrast(103%);
+            `;
+            document.documentElement.style.cssText = styles;
+            document.getElementsByClassName('qr-bg')[0].style.opacity = "0.06";
+            document.getElementsByClassName('qr-img')[0].style.background = "url(../img/loading-dark.gif)";
+            document.getElementsByClassName('chat-bg')[0].style.opacity = "0.06";
+        }
+        else {
+            let styles = `
+                --background-primary: #ffffff;
+                --background-secondary: #fffffff2;
+                --background-tertiary: #f0f2f5;
+                --background-fourth: #e4e5e9;
+                --background-fifth: #00000029;
+                --chat-background: #eae6df;
+                --border-primary: #ffffff;
+                --border-secondary: #d1d7db;
+                --text-primary: #111b21;
+                --text-secondary: #3b4a54;
+                --text-tertiary: #667781;
+                --icon-primary: #54656f;
+                --unseen-messages: #1fa855;
+                --message-background-primary: #d9fdd3;
+                --message-background-secondary: #ffffff;
+                --theme-mode: #1fa855;
+                --filter-primary: invert(99%) sepia(0%) saturate(0%) hue-rotate(137deg) brightness(109%) contrast(101%);
+                --filter-secondary: invert(89%) sepia(27%) saturate(292%) hue-rotate(61deg) brightness(105%) contrast(103%);
+            `;
+            document.documentElement.style.cssText = styles;
+            document.getElementsByClassName('qr-bg')[0].style.opacity = "0.4";
+            document.getElementsByClassName('qr-img')[0].style.background = "url(../img/loading-light.gif)";
+            document.getElementsByClassName('chat-bg')[0].style.opacity = "0.4";
+        }
+    });
+});
+
+/*
+function render(data) {
+    var html = data.map(function(elem, index) {
+        return(`
+            <div>
+                ${elem.author}
+                ${elem.text}
+            </div>
+        `);
+    }).join(" ");
+    document.getElementById('').innerHTML = html;
+}
+
+function addMessage(e) {
+    var payload = {
+        author: document.getElementById('').value,
+        text: document.getElementById('').value
+    }
+    socket.emit('new-messages', payload);
+    return false;
+}*/
